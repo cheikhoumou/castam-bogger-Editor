6e00972907c5da16659656dfc64f1c5d8a1f4197
Cheikh Oumou <67443473+cheikhoumou@users.noreply.github.com>
2021-10-24 19:54:56 +0200

    v1
    v1

--- data.js
+++ data.js
@@ -1,3 +1,7 @@
-console.log('data1');
-let username = document.getElementById("displayusername").innerHTML;
-localStorage.setItem(user,username);
+console.log('data');
+function camondata(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;    
+localStorage.setItem("user",username);
+localStorage.setItem("pass",password);
+}
